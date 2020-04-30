65e834bb2a39544540638ad53fd09f4c15c06181
parthberk <sh.prth@yahoo.com>
2020-04-29 19:34:43 -0700

    "Programming challange solution 2"

---
+++ Algorithms-UCSD/Programming Challenges/Maximum Pairwise Product/maximum_pairwise_product.py
@@ -0,0 +1,38 @@
+# python3
+
+import sys
+def max_pairwise_product_naive(numbers):
+    assert len(numbers) >= 2
+    assert all(0 <= x <= 2 * 10 ** 5 for x in numbers)
+
+    product = 0
+
+    for i in range(len(numbers)):
+        for j in range(i + 1, len(numbers)):
+            product = max(product, numbers[i] * numbers[j])
+
+    return product
+
+
+def max_pairwise_product(numbers):
+    assert len(numbers) >= 2
+    assert all(0 <= x <= 2 * 10 ** 5 for x in numbers)
+    max_num = -sys.maxsize
+    second_max = -sys.maxsize
+    for num in numbers:
+        if max_num < num:
+            second_max = max_num
+            max_num = num
+        else:
+            if num > second_max:
+                second_max = num
+    return max_num * second_max
+
+
+
+
+if __name__ == '__main__':
+    n = int(input())
+    input_numbers = list(map(int, input().split()))
+    assert len(input_numbers) == n
+    print(max_pairwise_product(input_numbers))
